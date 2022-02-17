1e99851fdad89e139918185c93691cae2b3ec8f3
ryoga <surpace2479@gmail.com>
2022-02-17 19:24:21 +0900

    add: simple communicate

--- main.cpp
+++ main.cpp
@@ -13,23 +13,71 @@ std::vector<DigitalOut> onboard_leds = {
 // シリアル通信用インスタンス
 static UnbufferedSerial *pc;
 
+void receivedData();
+
+// PCから送られてくる制御用データ
+std::string cmd_data = "";
+std::string cmd_data_buf = "";
+
+void printString(std::string str)
+{
+    pc->write(str.c_str(), str.length());
+}
+
+// 受信データをデコード
+std::vector<int> decodeReceivedData()
+{
+    // 文字を数値化
+    char cmd_data_char = cmd_data[0];
+    int data_int = (int)cmd_data_char - 33;
+
+    // pc->write(std::to_string(data_int).c_str(), 5);
+
+    // vectorに変換
+    std::vector<int> data(4);
+    for (int i = 0; i < 4; ++i) {
+        data[i] = (data_int >> i) & 1;
+    }
+
+    return data;
+}
+
 int main() 
 {
     uint8_t count = 0;
     std::string str = "He\r\n";
 
     while(1) 
-    {
+    {       
         pc = new UnbufferedSerial(USBTX, USBRX,9600);
-        
-        str = std::to_string(count) + "\n";
-        pc->write(str.c_str(), 7);  
-        
-        onboard_leds[0] = 1;
+        pc->attach(receivedData, SerialBase::RxIrq);
+
+        // 受信データのデコード
+        std::vector<int> rcv_data = decodeReceivedData();
+ 
+        for (int i = 0; i < onboard_leds.size(); ++i)
+        {
+            onboard_leds[i] = rcv_data[i];
+        }
         wait_us(500*1000);
-        onboard_leds[0] = 0;
+        
+        for (int i = 0; i < onboard_leds.size(); ++i)
+        {
+            onboard_leds[i] = 0;
+        }
         wait_us(500*1000);
 
-        ++count;
+        // ++count;
     }
+}
+
+// シリアルデータ受信割り込み
+void receivedData()
+{
+    char gotchar[10]={0,0,0,0,0,0,0,0,0,0};
+    
+    //読み取り
+    pc->read(gotchar, 5);
+    cmd_data = gotchar;
+    cmd_data = cmd_data[0];
 }
